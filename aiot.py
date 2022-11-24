7d24af211fcbeefcb2936284e9d7bf2a88494e6b
Teeraphat Kullanankanjana <ku.teeraphat_st@tni.ac.th>
2022-11-25 01:32:19 +0700

    Update aiot.py
    Import New Library
    + json (dumps function)
    Fix Welcome message
    - remove 'more detail' in class instance 
    + Change welcome function name 
    + Change github url in welcome  
    Fix Code
    + Add new function 
    + Add default parameter to function 
    Fix Comment

--- aiot.py
+++ aiot.py
@@ -7,6 +7,7 @@ try :
     from network import WLAN,STA_IF
     from dht import DHT11
     from neopixel import NeoPixel
+    from json import dumps
     
 except ImportError:
     print("Error: Library Not Found")
@@ -14,7 +15,8 @@ except ImportError:
 class AIOT():
     """
     Create instance and setup all AIoT pin
-    For more detail: https://github.com/PerfecXX/MicroPython-ESP32-AIoT-DevBoard
+    -----
+    <Your instance name> = AIOT()
     """
     def __init__(self):
         self.__buzzFreq = 500
@@ -44,21 +46,26 @@ class AIOT():
         self.__btn4Pin = Pin(4,Pin.IN,Pin.PULL_UP)
         self.__potPin = ADC(Pin(39))
         self.__ldrPin = ADC(Pin(36))
-        self.author()
+        self.__welcome()
         
-    def rgb_setColor(self,index,color):
+    def rgb_setColor(self,index='all',color=(255,255,255)):
         """
         Set an RGB color to a specific position
         -----
-        Parameter:
-        (int)index -> 0 or 1 => Position of RGB LED
+        Parameter
+        (int)index -> 0 or 1 => Position of RGB LED1 or LED2
+        (str)index -> 'all' => Set both positions of the RGB LED
         (tuple)color -> (r,g,b) => color of RGB LED
         ---
         (int)r -> 0 to 255 => intensity of the red color
         (int)g -> 0 to 255 => intensity of the green color
         (int)b -> 0 to 255 => intensity of the blue color
         """
-        self.__neopixelPin[index] = (color[0],color[1],color[2])
+        if index == 'all':
+            self.__neopixelPin[0] = (color[0],color[1],color[2])
+            self.__neopixelPin[1] = (color[0],color[1],color[2])
+        else:
+            self.__neopixelPin[index] = (color[0],color[1],color[2])
         
     def rgb_show(self):
         """
@@ -68,65 +75,151 @@ class AIOT():
         
     def rgb_off(self,index):
         """
-        Set "off" to a specific RGB LED position. 
+        Set "Off" to a specific RGB LED position. 
         -----
         Parameter
         (int)index-> 0 or 1 => Position of RGB LED   
         (str)index-> 'all' => All of RGB LED
         """
-        if index == 0:
-            self.__neopixelPin[0] = (0,0,0)
-        elif index ==1:
-            self.__neopixelPin[1] = (0,0,0)
-        elif index == 'all':
+        if index == 'all':
             self.__neopixelPin[0] = (0,0,0)
             self.__neopixelPin[1] = (0,0,0)
+        else:
+            self.__neopixelPin[index] = (0,0,0)
         self.rgb_show()
+        
+    def relay_on(self,index):
+        """
+        Set "On" to specific relay position
+        -----
+        Parameter
+        (int)index-> 1 or 2 => Position of Relay
+        (str)index-> 'all' => All of Relay
+        """
+        if index == 'all':
+            self.__relay1Pin.value(1)
+            self.__relay2Pin.value(1)
+        elif index == 1:
+            self.__relay1Pin.value(1)
+        elif index == 2:
+            self.__relay2Pin.value(1)
+    
+    def relay_off(self,index):
+        """
+        Set "Off" to specific relay position
+        -----
+        Parameter
+        (int)index-> 1 or 2 => Position of Relay
+        (str)index-> 'all' => All of Relay
+        """
+        if index == 'all':
+            self.__relay1Pin.value(0)
+            self.__relay2Pin.value(0)
+        elif index == 1:
+            self.__relay1Pin.value(0)
+        elif index == 2:
+            self.__relay2Pin.value(0)
+    
+    def relay_isOn(self,index):
+        """
+        Determines whether the specified relay is opened.
+        -----
+        Parameter
+        (str)index -> "all" => any relay
+        (int)index -> 1 or 2 => Name of the relay
+        ---
+        Return
+        True-> if the specified relay is opened
+        """
+        
+        self.__relay1State = self.__relay1Pin.value()
+        self.__relay2State = self.__relay2Pin.value()
+        
+        if index == 1:
+            if self.__relay1State == 1:
+                return True
+        elif index == 2:
+            if self.__relay2State == 1:
+                return True
+        elif index == 'all':
+            if self.__relay1State == 1 or self.__relay2State == 1:
+                return True
         
     def button_isPressed(self,index):
         """
         Determines whether the specified button is pressed.
         -----
         Parameter
+        (str)index -> "all" => any button 
         (int)index -> 1,2,3 or 4 => Name of the button
         ---
         Return
-        1-> if the specify button is pressed
-        0-> if the specify button is not pressed
+        True-> if the specified button is pressed
         """
+        
         self.__btn1State = self.__btn1Pin.value()
         self.__btn2State = self.__btn2Pin.value()
         self.__btn3State = self.__btn3Pin.value()
         self.__btn4State = self.__btn4Pin.value()
+        
         if index ==1:
-            return self.__btn1State
+            if self.__btn1State == 0:
+                return True
         elif index ==2:
-            return self.__btn2State
+            if self.__btn2State == 0:
+                return True
         elif index ==3:
-            return self.__btn3State
+            if self.__btn3State == 0:
+                return True
         elif index ==4:
-            return self.__btn4State
-        
-        
+            if self.__btn4State == 0:
+                return True
+        elif index == 'all':
+            if self.__btn1State == 0 or self.__btn2State == 0 or self.__btn3State == 0 or self.__btn4State == 0:
+                return True
     
-    def get_dht(self):
+    def dht_measure(self,environment='all'):
         """
+        Obtains the temperature(celsius) and humidity(%RH) measure by the DHT11 Sensor.
+        -----
+        Parameter
+        (str)environment -> "temperature","humidity" or 'all' => type of measurement 
+        ---
+        Return
+        (tuple)Temperature,Humdity -> if environment set to "all"
+        (int)Temperature -> if environment set to "temperature"
+        (int)Humidity -> if environment set to "humidity"
         """
         self.__dhtPin.measure()
-        self.temperature = self.__dhtPin.temperature()
-        self.humidity = self.__dhtPin.humidity()
-    def author(self):
+        self.__temperature = self.__dhtPin.temperature()
+        self.__humidity = self.__dhtPin.humidity()
+        if environment == 'all':
+            return self.__temperature,self.__humidity
+        elif environment == 'temperature':
+            return self.__temperature
+        elif environment == 'humidity':
+            return self.__humidity
+        
+    def display_setText(self,text='Test',xPostion=0,YPosition=0,color=1):
+        """
+        """
+        self.__oled.text(str(text),int(xPostion),int(YPosition),int(color))
+        
+    def display_show(self):
+        """
+        """
+        self.__oled.show()
+        
+    def __welcome(self):
         print('Welcome to KMITL AIoT Development Board Version 1')
         print('For more detail,please visit')
-        print('https://github.com/PerfecXX/MicroPython-ESP32-AIoT-DevBoard ')
+        print('https://github.com/PerfecXX/AIoT_MicroPythonAPI')
         
-        
+            
 board = AIOT()
-board.rgb_setColor(0,(255,0,0))
-board.rgb_setColor(1,(0,255,0))
+board.rgb_setColor('all',(255,0,0))
 board.rgb_show()
 sleep(1)
 board.rgb_off('all')
-if board.button_isPressed(1):
-    print('bnt1 press')
-        
+board.display_setText()
+board.display_show()
